1c2ac1c3ec9ab75eb60f98a20fe6be91b5cbeca8
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-03-06 18:06:01 +0100

    first version of the footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,7 +3,20 @@ export default {
     name: 'AppFooter',
     data() {
         return {
-            
+            footerLinks: [
+                {
+                    name: 'home',
+                    label: 'Home',
+                },
+                {
+                    name: 'cars',
+                    label: 'Shop',
+                },
+                {
+                    name: '',
+                    label: 'About Us',
+                },
+            ],
         }
     },
 }
@@ -23,26 +36,27 @@ export default {
                 </div>
 
                 <!-- Links section -->
-                <div class="col-4 py-5">
+                <div class="col-4 d-flex py-5">
                     <div class="col-6 text-white">
                         <h5>Services</h5>
                         <ul class="list-unstyled">
-                            <li>Services</li>
-                            <li>Services</li>
-                            <li>Services</li>
-                            <li>Services</li>
+                            <li><a class="text-decoration-none text-white" href="#">Terms & Conditions</a></li>
+                            <li><a class="text-decoration-none text-white" href="#">Privacy Policy</a></li>
+                            <li><a class="text-decoration-none text-white" href="#">Design</a></li>
+                            <li><a class="text-decoration-none text-white" href="#">Development</a></li>
                         </ul>
                         <h5>Contact Us</h5>
                         <ul class="list-unstyled text-white">
-                            <li>Services</li>
-                            <li>Services</li>
+                            <li><a class="text-decoration-none text-white" href="#">Email</a></li>
+                            <li><a class="text-decoration-none text-white" href="#">Location</a></li>
                         </ul>
                     </div>
                     <div class="col-6 text-white">
-                        <h5>Company</h5>
+                        <h5>Site</h5>
                         <ul class="list-unstyled text-white">
-                            <li>Services</li>
-                            <li>Services</li>
+                            <li v-for="link, index in footerLinks">
+                                <router-link class="text-decoration-none text-white" :to="{ name: link.name }">{{ link.label }}</router-link>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -51,12 +65,32 @@ export default {
                 <div class="col-4 d-flex justify-content-end py-5 text-white">
                     <div class="">
                         <h5>Get in touch</h5>
-                        <i class="fab fa-facebook"></i>
-                        <i class="fab fa-twitter"></i>
-                        <i class="fab fa-linkedin-in"></i>
-                        <h5>Subscribe</h5>
-                        
+                        <div class="my-3">
+                            <a class="text-white" href="">
+                                <i class="fab fa-facebook border rounded-5 border-white p-2 me-2"></i>
+                            </a>
+                            <a class="text-white" href="">
+                                <i class="fab fa-twitter border rounded-5 border-white p-2 me-2"></i>
+                            </a>
+                            <a class="text-white" href="">
+                                <i class="fab fa-linkedin-in border rounded-5 border-white p-2 me-2"></i>
+                            </a>
+                        </div>
+                        <div class="mt-5">
+                            <h5 class="pb-3">Subscribe</h5>
+                            <div class="d-flex">
+                                <input type="email" class="form-control rounded me-3 pe-5 " id="newsletter" placeholder="Enter email address">
+                                <button class="rounded sub_btn text-white px-4" type="button">Subscribe</button>
+                            </div>
+                        </div>
                     </div>
+                </div>
+
+                <hr class="text-white">
+
+                <!-- Copyright -->
+                <div class="col-12 d-flex justify-content-center my-3 text-white">
+                    <span>&#169 Made with &#9829 by Team 7</span>
                 </div>
 
             </div>
@@ -73,6 +107,11 @@ footer{
 
     img{
         width: 50%
+    }
+
+    .sub_btn{
+        background-color: $my_red;
+        border-color: $my_red;
     }
 }
 
