ec2325110629fa6782b346373fe660a836f6b123
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-03-06 11:55:26 +0100

    added logo in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,17 +3,58 @@ export default {
     name: 'AppHeader',
     data() {
         return {
-            
+            navLinks: [
+                {
+                    name: 'home',
+                    label: 'Home',
+                },
+                {
+                    name: 'shop',
+                    label: 'Shop',
+                },
+                
+            ],
         };
     },
 }
 </script>
 
 <template>
-    <div>
 
+    <div class="hr_bg">
+        <div class="container">
+            <div class="row">
+
+                <!-- Logo -->
+                <div class="col-4">
+                    <img class="img_logo py-2" src="../assets/logo.png" alt="">
+                </div>
+
+
+                <div class="col-4">
+
+                </div>
+                
+                <div class="col-4">
+
+                </div>
+            </div>
+        </div>
     </div>
+
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+.hr_bg{
+    background-color: #161d20;
+    height: 80px;
+
+    .img_logo{
+        height: 80px;
+        width: auto;
+    }
+
+}
+
 </style>
